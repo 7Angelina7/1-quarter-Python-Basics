de41f1b124d67bc58c860414675cedf90c011124
Angelina <kro-angelina@yandex.ru>
2022-02-13 23:30:12 +0300

    lesson1

---
+++ lesson1/1.py
@@ -0,0 +1,23 @@
+name = "Tom"
+age = 16
+complaints = "pain"
+temperature = 39
+
+print(name)
+print(age)
+print(complaints)
+print(temperature)
+
+name = input()
+print("введите имя:", name)
+age = input()
+print("введите возраст:", age)
+complaints = input()
+print("введите жалобы:", complaints)
+temperature = input()
+print("введите температуру тела:", temperature)
+
+
+
+
+
---
+++ lesson1/3.py
@@ -0,0 +1,6 @@
+a = input("Введите число n:")
+a = int(a)
+b = a * 11
+c = a * 111
+d = a + b + c
+print("сумма чисел n + nn + nnn =", d)
